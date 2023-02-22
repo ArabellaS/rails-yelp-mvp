083c8022b922a1aa3d8d800bbe5881a4bc6b5af3
Cecile Saiz <cecile.saiz@gmail.com>
2023-02-22 15:26:53 +0100

    Yelp MVP finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :restaurants, except: [:update, :edit, :destroy] do
+    resources :reviews, only: [:create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+mongelli = {name: "Mongelli", address: "Toulouse", phone_number: "+3356734526272", category: "italian"}
+sales_gosses =  {name: "Les sales Gosses", address: "rue de l'Industrie", phone_number: "+3356734526256", category: "french"}
+ramen = {name: "Shin-Ya", address: "Toulouse", phone_number: "+3356734526253", category: "japanese"}
+etoiles = {name: "Etoiles d'Asie", address: "Toulouse", phone_number: "+3356734526290", category: "chinese"}
+frites = {name: "Friterie", address: "Toulouse", phone_number: "+3356734526239", category: "belgian"}
+
+[mongelli, sales_gosses, ramen, etoiles, frites].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
